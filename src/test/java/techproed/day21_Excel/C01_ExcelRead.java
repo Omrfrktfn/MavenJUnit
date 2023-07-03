916793764aa6c11083df61ed3187659865872f39
Omer <tufanfaruk71@gmail.com>
2023-07-03 12:29:22 +0200

    03/07

---
+++ src/test/java/techproed/day21_Excel/C01_ExcelRead.java
@@ -0,0 +1,86 @@
+package techproed.day21_Excel;
+
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Assert;
+import org.junit.Test;
+import utilities.TestBase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class C01_ExcelRead extends TestBase {
+
+    @Test
+    public void test01() throws IOException {
+
+
+        //3. satir 1. sutun degerini yazdirin ve "France" oldugunu test edin.
+
+        //1-dosya yolunu aliriz = src/test/java/resources/Capitals (2).xlsx
+        String path = "src/test/java/resources/Capitals (1).xlsx";
+
+        //2- olusturulan doyayi sisteme getirme islemi yapilir.
+        FileInputStream fis = new FileInputStream(path);
+
+        //3- dosyayi workbook'a atamaliyiz. java ortaminda bir excel dosyasi olusturmaliyiz.
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        //4-sayfayi (sheet) secmeliyiz.
+        Sheet sheet = workbook.getSheet("Sheet1");
+
+        //5- satiri (row) secmeliyiz.
+        Row row = sheet.getRow(3); // birinci satir index 0'dan baslar
+
+        //hucreyi(cell) secmeliyi.z
+        Cell cell = row.getCell(1);
+
+        String satir3Sutun1 = workbook.getSheet("Sheet1").getRow(2).getCell(0).toString();
+        System.out.println(satir3Sutun1);
+
+        Assert.assertEquals("France", satir3Sutun1);
+
+
+        //son satir sayisini bulunuz.
+        int sonSatirSayisi = workbook.getSheet("Sheet1").getLastRowNum();
+        System.out.println("Son Satir Sayisi : " + sonSatirSayisi);
+
+        //kullanilan satir sayisini bulunuz.
+        int kullanilanSatirSayisi = workbook.getSheet("Sheet1").getPhysicalNumberOfRows();
+        System.out.println("Kullanilan Satir Sayisi : " + kullanilanSatirSayisi);
+
+        //excell tablosundaki kullanilan satir sayisini getPhysicalNumberOfRows methodu ile alabiliriz.
+        //(index olarak vermez.)
+
+    }
+
+
+    @Test
+    public void test02() throws IOException {
+
+        //Capital dosyasindaki tum verileri yazdirirniz
+
+        Map<String, String> capitalsMap = new HashMap<>();
+        //Capitals dosyasındaki tüm verileri koyabilecegimiz en uygun java objesi map'dir.
+
+        String dosyaYolu =  "src/test/java/resources/Capitals (1).xlsx";
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        int sonSatirSayisi = workbook.getSheet("Sheet1").getLastRowNum();
+
+        for (int i = 0; i <= sonSatirSayisi; i++) {
+            String key = workbook.getSheet("Sheet1").getRow(i).getCell(0).toString();
+            String value = workbook.getSheet("Sheet1").getRow(i).getCell(1).toString();
+
+            capitalsMap.put(key, value);
+
+         //   System.out.println(key+"/"+value);
+
+        }
+        System.out.println(capitalsMap);
+
+    }
+}
---
+++ src/test/java/techproed/day21_Excel/C02_ExcellWrite.java
@@ -0,0 +1,46 @@
+package techproed.day21_Excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+import utilities.TestBase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class C02_ExcellWrite {
+
+    @Test
+    public void test01() throws IOException {
+
+        //Bir nufus sutunu olusturun
+        String path = "src/test/java/resources/Capitals (1).xlsx";
+
+        FileInputStream fis = new FileInputStream(path);
+
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        //baskent nufuslarini excell dosyasina yaziniz.
+
+        workbook.getSheet("Sheet1").getRow(0).createCell(2).setCellValue("NUFUS");
+        workbook.getSheet("Sheet1").getRow(1).createCell(2).setCellValue("10000");
+        workbook.getSheet("Sheet1").getRow(2).createCell(2).setCellValue("11000");
+        workbook.getSheet("Sheet1").getRow(3).createCell(2).setCellValue("12000");
+        workbook.getSheet("Sheet1").getRow(4).createCell(2).setCellValue("13000");
+        workbook.getSheet("Sheet1").getRow(5).createCell(2).setCellValue("14000");
+
+        //excell'de bir hucre olusturmak icin createCell methodu kullanilir.
+        //Hucre icine yazdiracagimiz deger icin setCellValue methdou kullanilir.
+
+        FileOutputStream fos = new FileOutputStream(path);
+        //datalari bizim class'imizdan capitals dosyasina gondericegiz.
+
+        workbook.write(fos);
+        //workbook'taki datalari fos'a yazdirdik.
+
+
+
+    }
+}
